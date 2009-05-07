707e5cc4ad9e7ce3656e4a3881e300268515312d
stocktwits <stocktwits@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-07 04:19:22 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/stocktwits-ticker-links/trunk@116078 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ stocktwits_ticker_links.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: StockTwits Ticker Links
+Plugin URI: http://www.stocktwits.com/
+Version: 1.0
+Author: StockTwits
+Author URI: http://stocktwits.com/
+Description: Plugin links stock tickers such as $AAPL with the realtime discussions about the stock at <a href="http://www.stocktwits.com/">StockTwits.com</a>.
+*/
+
+function process_content ($content)
+{
+   $content =
+      preg_replace (
+         '/\$((?:[0-9]+(?=[a-z])|(?![0-9\.\:\_\-]))(?:[a-z0-9]|[\_\.\-\:](?![\.\_\.\-\:]))*[a-z0-9]+)/i',
+         "<a href=\"http://www.stocktwits.com/t/$1\" class=\"ticker\" target=\"new\"><span>\$</span>$1</a>",
+         $content
+         );
+
+     return $content;
+}
+
+add_filter ('the_content',       'process_content');
+add_filter ('the_content_limit', 'process_content');
+add_filter ('the_excerpt',       'process_content');
+add_filter ('the_content_rss',   'process_content');
+add_filter ('the_excerpt_rss',   'process_content');
+
+?>
